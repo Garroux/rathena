e67f0f28260bb1ee249bb518c7102e4ad74f429e
Eduardo Garroux <40306002+Garroux@users.noreply.github.com>
2026-08-11 20:24:43 -0300

    change droprate and all items identified

--- src/map/itemdb.cpp
+++ src/map/itemdb.cpp
@@ -3342,15 +3342,15 @@ bool itemdb_ishatched_egg( const item* item ) {
 */
 char itemdb_isidentified(t_itemid nameid) {
 	int32 type=itemdb_type(nameid);
-	switch (type) {
-		case IT_WEAPON:
-		case IT_ARMOR:
-		case IT_PETARMOR:
-		case IT_SHADOWGEAR:
-			return 0;
-		default:
+	//switch (type) {
+		//case IT_WEAPON:
+		//case IT_ARMOR:
+		//case IT_PETARMOR:
+		//case IT_SHADOWGEAR:
+		//	return 1;
+		//default:
 			return 1;
-	}
+	//}
 }
 
 const std::string ItemGroupDatabase::getDefaultLocation() {
